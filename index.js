678713e2ac3a2158ce610eb73de5da8cb88becf5
Kathlene Tajonera <kathlene.tajonera@gmail.com>
2021-06-19 13:39:29 +0800

    ui done

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const nav = document.querySelector(".nav");
 const navToggle = document.querySelector(".nav__toggle");
+let windowWidth = window.innerWidth;
 
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("nav--active");
+});
+
+window.addEventListener("resize", () => {
+  if (windowWidth !== window.innerWidth) {
+    windowWidth = window.innerWidth;
+
+    if (windowWidth >= 768) {
+      nav.classList.remove("nav--active");
+    }
+  }
 });
